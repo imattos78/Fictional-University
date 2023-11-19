52545832e46678b4a3e794712e48fdb8c37d2f3d
Nacho <nachomattos78@gmail.com>
2023-11-19 13:39:43 +0000

    Post and Page page added / Config bar added

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -1,12 +1,13 @@
-<?php get_header();
+<?php 
+    get_header();
+    
     while(have_posts()) {
         the_post(); ?>
-        <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="title"><?php the_title(); ?></a></h2>
         <div class="desc"><?php the_content() ?></div>
-        <hr>
+        
         <?php
     }
 
     get_footer();
 ?>
-

7a2cd63740d3a4b5a5fae7e6382f19d09e6f7220
Nacho <nachomattos78@gmail.com>
2023-12-17 12:09:22 +0000

    updated past events configuration

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -15,6 +15,7 @@ get_header(); ?>
     <?php
          $today = date('Ymd'); 
          $pastEvents = new WP_Query(array(
+          'paged' => get_query_var('paged', 1),
            'post_type' => 'event',
            'meta_key' => 'event_date',
            'orederby' => 'meta_value_num',
@@ -44,7 +45,9 @@ get_header(); ?>
                 </div>
             </div>
       <?php } 
-      echo paginate_links();      ?>
+      echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+      ));      ?>
 
     </div>
 
